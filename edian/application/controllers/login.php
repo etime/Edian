55269f80b3f549d00976016c62e0d68b340373e7
JMJST <chengfeng1992@hotmail.com>
2013-12-08 22:21:17 +0800

    为了获得最新的网站版本，不得不 commit 一下

--- edian/application/controllers/login.php
+++ edian/application/controllers/login.php
@@ -40,62 +40,68 @@ class Login extends CI_Controller {
 
     /**
      * 用于用户登录的各种检测，供 ajax 和内部调用
-     * 我是这样想的，不管用户登录出现什么样的错误，只返回一个错误信息：您的登录名和密码不匹配
+     * 我是这样想的，不管用户登录出现什么样的错误，只返回一个错误信息：您的登录名和密码不匹配，同时做出如下约定：
+     *     -1    表示用户登录失败
+     *      1    表示使用 loginName 登录
+     *      2    表示使用 phoneNum 登录
+     * @param string $userName
+     * @param string $password
+     * @return int
      */
-    public function checkMatch($userName = false, $password = '') {
-        if ($userName == false) {
+    public function checkMatch($userName = '', $password = '') {
+        if ($userName == '') {
             $userName = $this->input->post('userName');
             $password = $this->input->post('password');
         }
 
         // 先假定用户的登录情况合法
-        $ans = true;
+        $ans = 3;
 
         // 输入不能为空
         if ($userName == '' || $password == '') {
-            $ans = false;
+            $ans = -1;
         }
 
         // 对用户的 userName 进行分类讨论
         if ($this->_isPhoneNum($userName) == true) {
             if (! $this->user->isUserExistByPhone($userName)) {
                 // 用户不存在
-                $ans = false;
+                $ans = -1;
             } else {
                 $expectPassword = $this->user->getUserPasswordByPhone($userName);
                 // 用户输入密码与其登陆密码不符
                 if ($password != $expectPassword) {
-                    $ans = false;
+                    $ans = -1;
+                } else {
+                    $ans = 2;
                 }
             }
         } else if ($this->_isLoginName($userName) == true) {
             if (! $this->user->isUserExistByLoginName($userName)) {
                 // 用户不存在
-                $ans = false;
+                $ans = -1;
             } else {
                 $expectPassword = $this->user->getUserPasswordByLoginName($userName);
                 // 用户输入密码与其登陆密码不符
                 if ($password != $expectPassword) {
-                    $ans = false;
+                    $ans = -1;
+                } else {
+                    $ans = 1;
                 }
             }
         } else {
             // $loginName 是其他情况
-            $ans = false;
+            $ans = -1;
         }
 
-        if (! $ans) {
-            if (isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) == 'xmlhttprequest') {
-                echo "false";
+        if (isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) == 'xmlhttprequest') {
+            if ($ans == -1) {
+                echo 'false';
             } else {
-                return false;
+                echo 'true';
             }
         } else {
-            if (isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) == 'xmlhttprequest') {
-                echo "true";
-            } else {
-                return true;
-            }
+            return $ans;
         }
     }
 
@@ -108,13 +114,22 @@ class Login extends CI_Controller {
         $userName = $this->input->post('userName');
         $password = $this->input->post('password');
         $url = $this->input->post('target');
-        if ($this->checkMatch($userName, $password)) {
-            // 用户登录成功，只可能是两种情况：loginName 登录和 phoneNum 登录
-            if ($this->_isLoginName($userName)) {
+
+        // 如果上次用户访问的页面的 url 为空，登录成功之后自动跳转到首页
+        if ($url == ' ') {
+            $url = site_url();
+        }
+
+        // 进行登录验证
+        $flag = $this->checkMatch($userName, $password);
+
+        // 对返回的错误代码进行分析
+        if ($flag != -1) {
+            // 用户登录成功，只可能是两种情况：loginName(1) 登录和 phoneNum(2) 登录
+            if ($flag == 2) {
+                $this->session->set_userdata('userId', $this->user->getuserIdByPhone($userName));
+            } else if ($flag == 1) {
                 $this->session->set_userdata('userId', $this->user->getUserIdByLoginName($userName));
-            }
-            else if ($this->_isPhoneNum($userName)) {
-                $this->sesion->set_userdata('userId', $this->user->getuserIdByPhone($userName));
             }
             // 跳转到用户登录之前访问的页面
 			header("Location: $url");
